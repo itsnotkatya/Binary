74d0c81c2e2dcbb5eeada27fa26a5f1b83e0f10b
itsnotkatya <49903775+itsnotkatya@users.noreply.github.com>
2019-11-23 08:35:51 +1000

    Second commit

---
+++ cmake-build-debug/CMakeFiles/homework_01.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ekaterina/CLionProjects/homework_01/functions.cpp" "/Users/ekaterina/CLionProjects/homework_01/cmake-build-debug/CMakeFiles/homework_01.dir/functions.cpp.o"
+  "/Users/ekaterina/CLionProjects/homework_01/main.cpp" "/Users/ekaterina/CLionProjects/homework_01/cmake-build-debug/CMakeFiles/homework_01.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/homework_01.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework_01.dir/main.cpp.o"
+  "CMakeFiles/homework_01.dir/functions.cpp.o"
+  "homework_01.pdb"
+  "homework_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
